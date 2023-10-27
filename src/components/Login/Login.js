53b67c64ec37fefe5bcfcd8d2d46ae3af422b6ab
quoc loc <loc842211@gmail.com>
2023-10-27 21:41:14 +0700

    tao form mobe/laptop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,10 @@
 import './Login.scss'
 const Login = (props)=>{
     return (
-         <div className="login-container mt-3">
+         <div className="login-container">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
                         <div className='brand'>
                             Quoc Loc
                         </div>
@@ -12,11 +12,16 @@ const Login = (props)=>{
                             Tranquocloc
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+                    <div className='brand d-sm-none'>
+                            Quoc Loc
+                        </div>
                         <input type='text' className='form-control' placeholder='Email address or phone number'/>
                         <input type='password' className='form-control'placeholder='Password'/>
                         <button className='btn btn-primary'>Login</button>
-                        <span className='text-center'>Forgot you password?</span>
+                        <span className='text-center'>
+                            <a className='forgot-password' href='#'>Forgot you password?</a>
+                            </span>
                         <hr/>
                         <div className='text-center'>
                         <button className='btn btn-success'>Create new account</button>
